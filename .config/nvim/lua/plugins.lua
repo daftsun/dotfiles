ede1c3d86762938531cc9b69ebe9bbb4b6208fc7
Aditya Gupta <aditya27gupta@gmail.com>
2026-05-01 13:15:37 +0530

    Update neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -11,6 +11,16 @@ vim.pack.add({
 
 -- Icons
 require('mini.icons').setup()
+-- Git
+require('mini.git').setup()
+
+-- Git Diff
+require('mini.diff').setup({
+  view = {
+    style = 'sign',
+    signs = { add = '+', change = '~', delete = '-' },
+  },
+})
 
 -- Statusline
 require('mini.statusline').setup({
@@ -18,10 +28,10 @@ require('mini.statusline').setup({
     active = function()
       local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 50 })
       local git           = MiniStatusline.section_git({ trunc_width = 40 })
-      local filename      = MiniStatusline.section_filename({ trunc_width = 50 })
+      local filename = MiniStatusline.section_filename({ trunc_width = 50 })
       local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 250 })
       local location      = '%l:%v-%L'
-      local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+      local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
 
       return MiniStatusline.combine_groups({
